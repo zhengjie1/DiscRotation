8174d42026052797eaef5f9b9501abd554b8704c
as <as@asdeiMac.local>
2015-03-31 09:20:30 +0800

    Initial Commit

---
+++ DiscRotation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiscRotation
+//
+//  Created by as on 15/3/31.
+//  Copyright (c) 2015年 as. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
